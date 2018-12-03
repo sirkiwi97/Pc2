5f1dbc4842eb37957cfed51e680ac7765064afc7
josue <josuetrive.29@outlook.com>
2018-12-03 14:22:52 -0500

    Added Pregunta2 Signed-off-by: josue <josuetrive.29@outlook.com>

--- Pregunta2.py
+++ Pregunta2.py
@@ -8,6 +8,15 @@ def orden_menor(lista):
     for i in lista:
         if i < menor:
             menor=i
+    m.append(menor)
+    n.remove(menor)
+    for i in lista:
+        if i < menor:
+            menor=i
+    m.append(menor)
+    n.remove(menor)        
     return menor
 menor=orden_menor(n)
 print (menor)
+print(n)
+print (m)
