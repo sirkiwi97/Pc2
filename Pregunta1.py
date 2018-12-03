f94e239300188f4c77c9cf00a73e372c8a9c3b15
josue <josuetrive.29@outlook.com>
2018-12-02 19:42:40 -0500

    Added Pregunta 1 file
    Signed-off-by: josue <josuetrive.29@outlook.com>

--- Pregunta1.py
+++ Pregunta1.py
@@ -4,5 +4,6 @@ y=int(input("coordenada y"))
 
 distanciax=0+x
 distanciay=0+y
-
-print ("el punto","(",x,",",y,")", "tiene una distancia del punto (0,0) de", distanciax,"en el eje x y",distanciay,"en el eje y")
+distanciaxy=(x**2+y**2)**0.5
+print ("el punto","(",x,",",y,")", "tiene una distancia del punto (0,0) de", distanciax,"en el eje x y",distanciay,"en el eje y",
+"distacia punto",distanciaxy)
